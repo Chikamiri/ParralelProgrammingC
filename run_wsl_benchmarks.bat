855c546fe84cdd697fa2cd99cec82ba89dfa92f8
Chikamiri <bach.px@outlook.com>
2025-11-10 00:00:47 +0700

    cleanup

--- run_wsl_benchmarks.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Running benchmark inside Ubuntu...
-
-set WIN_PATH=%CD%
-for /f "delims=" %%i in ('wsl -d Ubuntu wslpath "%WIN_PATH%"') do set WSL_PATH=%%i
-
-wsl -d Ubuntu fish -c "cd '%WSL_PATH%' && fish ./benchmark.sh"
